8094ec06c4214ccb42baa0d82397cf367ab29b2f
Bhavy <bhavyb1234@gmail.com>
2025-09-01 02:24:06 +0530

    Updated

--- crafted/urls.py
+++ crafted/urls.py
@@ -5,19 +5,23 @@ from django.conf.urls.static import static
 import os
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from api.models import CustomUser  
 
-# Create superuser view
 def create_superuser(request):
-    # Get superuser details from environment variables
-    superuser_name = os.getenv('SUPERUSER_USERNAME', 'admin')
-    superuser_email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
-    superuser_password = os.getenv('SUPERUSER_PASSWORD', 'yourpassword')
+    try:
+        superuser_name = os.getenv('SUPERUSER_USERNAME', 'admin')
+        superuser_email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
+        superuser_password = os.getenv('SUPERUSER_PASSWORD', 'yourpassword')
 
-    # Check if superuser already exists
-    if not User.objects.filter(username=superuser_name).exists():
-        User.objects.create_superuser(superuser_name, superuser_email, superuser_password)
-        return HttpResponse(f"Superuser {superuser_name} created successfully!")
-    return HttpResponse(f"Superuser {superuser_name} already exists.")
+       
+        if not CustomUser.objects.filter(username=superuser_name).exists():
+            
+            CustomUser.objects.create_superuser(superuser_name, superuser_email, superuser_password)
+            return HttpResponse(f"Superuser {superuser_name} created successfully!")
+        return HttpResponse(f"Superuser {superuser_name} already exists.")
+    
+    except Exception as e:
+        return HttpResponse(f"Error occurred: {str(e)}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
